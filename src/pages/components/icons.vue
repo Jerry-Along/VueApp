06f2ecf401fdda4cb2f5386fdda72075af535ee0
DESKTOP-9PIVI1O\ALong <gglong8@163.com>
2019-06-16 23:07:08 +0800

    icons 图标适配

--- src/pages/components/icons.vue
+++ src/pages/components/icons.vue
@@ -1,12 +1,13 @@
 <template>
 	<div class="icons">
-		<div class="icon" v-for="ico in iconList" :key="ico.id">
-			<div class="icon-imgs">
-				<img class="icon-img" :src="ico.iconUrl"/>
+		<div class="icon" v-for="item of iconList" :key="item.id">
+			<div class="icon-img">
+				<img  class="icon-img-content" :src="item.iconUrl" />
 			</div>
-			<span class="icon-text">{{ico.name}}</span>
+			<p class="icon-text">{{item.name}}</p>
 		</div>
 	</div>
+	
 </template>
 
 <script>
@@ -16,36 +17,40 @@ export default{
 		return {
 			iconList:[{
 				id:"0001",
-				name:"频道1名称",
+				name:"门票1",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9facf35be.jpg"
 			},{
 				id:"0002",
-				name:"频道2名称",
+				name:"门票2",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f143f5f3.jpg"
 			},{
 				id:"0003",
-				name:"频道3名称",
+				name:"门票3",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9ecdb788a.jpg"
 			},{
 				id:"0004",
-				name:"频道4名称",
+				name:"门票4",
 				iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-08-31/5b88a551bad04.png"
 			},{
 				id:"0005",
-				name:"频道5名称",
+				name:"门票5",
 				iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-11-15/5becc5ff55398.jpg"
 			},{
 				id:"0006",
-				name:"频道6名称",
+				name:"门票6",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f27d09e8.jpg"
 			},{
 				id:"0007",
-				name:"频道7名称",
+				name:"门票7",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f649bc25.jpg"
 			},{
 				id:"0008",
-				name:"频道8名称",
+				name:"门票8",
 				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9fc863701.jpg"
+			},{
+				id:"0009",
+				name:"门票9",
+				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f41dd4bd.jpg"
 			}]
 		}
 	}
@@ -59,31 +64,23 @@ export default{
 		height:0
 		padding-bottom:50%
 		.icon
-			overflow: hidden
+			position:relative
 			float:left
-			width:25% 
+			width:25%
 			height:0
 			padding-bottom:25%
-			.icon-imgs
-				top:0
-				right:0
-				bottom:.4rem
-				left:0
+			.icon-img
+				position:absolute
 				box-sizing:border-box
-				padding:.1rem
-				.icon-img
-					display:block
-					margin:0 auto
-					width:100%
+				padding:0.1rem
+				.icon-img-content
+					width:80%
 			.icon-text
-				right:0
-				bottom:0
 				left:0
-				top:-3rem
-				height:1rem
-				line-height:1rem
-				background:#f00
+				bottom:0
+				right:0
+				position:absolute
+				font-size:17px
 				
-			
-			
+				
 </style>
