b6a6006da8ca7978c6d51b6a3b924f82b228de40
DESKTOP-9PIVI1O\ALong <gglong8@163.com>
2019-06-17 00:19:30 +0800

    取消自动轮播

--- src/pages/components/icons.vue
+++ src/pages/components/icons.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="icons">
-		<swiper>
+		<swiper :options="swiperOption">
 			<swiper-slide v-for="(page, index) in pages" :key="index">
 				<div class="icon" v-for="item of page" :key="item.id">
 					<div class="icon-img">
@@ -18,43 +18,47 @@ export default{
 	name:"homeIcons",
 	data(){
 		return {
-			iconList:[{
-				id:"0001",
-				name:"门票1门票2门票3门票4门票5",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9facf35be.jpg"
-			},{
-				id:"0002",
-				name:"门票2",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f143f5f3.jpg"
-			},{
-				id:"0003",
-				name:"门票3",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9ecdb788a.jpg"
-			},{
-				id:"0004",
-				name:"门票4",
-				iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-08-31/5b88a551bad04.png"
-			},{
-				id:"0005",
-				name:"门票5",
-				iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-11-15/5becc5ff55398.jpg"
-			},{
-				id:"0006",
-				name:"门票6",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f27d09e8.jpg"
-			},{
-				id:"0007",
-				name:"门票7",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f649bc25.jpg"
-			},{
-				id:"0008",
-				name:"门票8",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9fc863701.jpg"
-			},{
-				id:"0009",
-				name:"门票9",
-				iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f41dd4bd.jpg"
-			}]
+			iconList:[
+				{
+					id:"0001",
+					name:"门票1门票2门票3门票4门票5",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9facf35be.jpg"
+				},{
+					id:"0002",
+					name:"门票2",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f143f5f3.jpg"
+				},{
+					id:"0003",
+					name:"门票3",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9ecdb788a.jpg"
+				},{
+					id:"0004",
+					name:"门票4",
+					iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-08-31/5b88a551bad04.png"
+				},{
+					id:"0005",
+					name:"门票5",
+					iconUrl:"https://cs.vmovier.com/Uploads/Series/2018-11-15/5becc5ff55398.jpg"
+				},{
+					id:"0006",
+					name:"门票6",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f27d09e8.jpg"
+				},{
+					id:"0007",
+					name:"门票7",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f649bc25.jpg"
+				},{
+					id:"0008",
+					name:"门票8",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9fc863701.jpg"
+				},{
+					id:"0009",
+					name:"门票9",
+					iconUrl:"http://cs.vmovier.com/Uploads/Series/2017-06-22/594b9f41dd4bd.jpg"
+				}],
+				swiperOption: {
+					loop: false
+				},
 		}
 	},
 	computed:{
@@ -74,12 +78,12 @@ export default{
 </script>
 
 <style lang="stylus" scoped>
-	.icons
-		overflow:hidden
+	.icons >>> .swiper-container
 		width:100%
 		height:0
 		padding-bottom:50%
 		.icon
+			overflow:hidden
 			position:relative
 			float:left
 			width:25%
